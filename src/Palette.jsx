405bc97f06293828b47778405fb5cc077f62027a
SomeYellowGuy <>
2025-02-14 19:33:12 +0530

    changed version number

--- src/Palette.jsx
+++ src/Palette.jsx
@@ -100,7 +100,7 @@ function Palette(props) {
                         textAlign: "right",
                         float: "right",
                         marginRight: "3px"
-                    }}>v1.5.0</b>
+                    }}>v1.6.0</b>
                 </div>
             </div>
         </div>
